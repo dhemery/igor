ae7d40269ef4b2632107f14658f4d7e28908beab
Dale Emery <dale@dhemery.com>
2011-11-17 16:41:04 -0800

    Conform to updated selector engine protocol (0.8.4)

--- extension/IgorSelectorEngine.m
+++ extension/IgorSelectorEngine.m
@@ -6,16 +6,18 @@
 //  Copyright (c) 2011 Dale H. Emery. All rights reserved.
 //
 
+#undef DEBUG
 #import "IgorSelectorEngine.h"
 #import "Igor.h"
 
 @implementation IgorSelectorEngine
 +(void)load {
     [SelectorEngineRegistry registerSelectorEngine:[IgorSelectorEngine new] WithName:@"Igor"];
-    
+    NSLog(@"Registered IgorSelectorEngine as Igor");
 }
 
-- (NSArray *) selectViewsUsingShelleyWithSelector:(NSString *)selector {
+- (NSArray *) selectViewsWithSelector:(NSString *)selector {
+    NSLog(@"Igor selecting views for selector %@", selector);
     return [Igor selectViewsThatMatchQuery:selector];
 }
 
