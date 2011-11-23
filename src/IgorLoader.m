be7b294403853d4c32598737b673c8250799a534
Dale Emery <dale@dhemery.com>
2011-11-23 00:41:11 -0800

    Shuffle files around

--- src/IgorLoader.m
+++ src/IgorLoader.m
@@ -1,13 +1,19 @@
 #import "Igor.h"
 #import "SelectorEngineRegistry.h"
 
+#define DEBUG 1
+
 @interface IgorLoader
+
 +(void)load;
+
 @end
 
 @implementation IgorLoader
+
 +(void)load {
     [SelectorEngineRegistry registerSelectorEngine:[Igor new] WithName:@"igor"];
+    NSLog(@"Igor selector engine registered with Frank as 'igor'. Woo hoo!");
 }
 @end
 
