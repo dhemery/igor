59d21571c031902853d25f30511d74476a2b0d40
Dale Emery <dale@dhemery.com>
2012-04-10 23:56:32 -0700

    Consolidate RelationshipMatcher tests into ComplexMatcher.

--- test/ComplexMatcherTests.m
+++ test/ComplexMatcherTests.m
@@ -4,6 +4,7 @@
 #import "InstanceMatcher.h"
 #import "IdentityMatcher.h"
 #import "FalseMatcher.h"
+#import "UniversalMatcher.h"
 
 @interface ComplexMatcherTests : SenTestCase
 @end
@@ -12,6 +13,9 @@
     UIButton *root;
     UIButton *middle;
     UIButton *leaf;
+    IdentityMatcher *_matchesRoot;
+    IdentityMatcher *_matchesMiddle;
+    IdentityMatcher *_matchesLeaf;
 }
 
 - (void)setUp {
@@ -20,40 +24,61 @@
     leaf = [ViewFactory buttonWithAccessibilityHint:@"leaf"];
     [root addSubview:middle];
     [middle addSubview:leaf];
+    _matchesRoot = [IdentityMatcher forView:root];
+    _matchesMiddle = [IdentityMatcher forView:middle];
+    _matchesLeaf = [IdentityMatcher forView:leaf];
 }
 
-- (void)testMatchingSubject {
-    id<SubjectMatcher> matchesMiddle = [IdentityMatcher forView:middle];
-
-    ComplexMatcher *middleAnywhere = [ComplexMatcher withSubject:matchesMiddle];
+- (void)testSubjectMatchesView {
+    ComplexMatcher *middleAnywhere = [ComplexMatcher withSubject:_matchesMiddle];
 
     assertThat(middleAnywhere, [MatchesView view:middle inTree:root]);
 }
 
-- (void)testMismatchingSubject {
-    id<SubjectMatcher> matchesMiddle = [IdentityMatcher forView:middle];
-
-    ComplexMatcher *middleAnywhere = [ComplexMatcher withSubject:matchesMiddle];
+- (void)testSubjectMismatchesView {
+    ComplexMatcher *middleAnywhere = [ComplexMatcher withSubject:_matchesMiddle];
 
     assertThat(middleAnywhere, isNot([MatchesView view:leaf inTree:root]));
 }
 
-- (void)testMatchingSubjectMatchingHead {
-    id<SubjectMatcher> matchesRoot = [IdentityMatcher forView:root];
-    id<SubjectMatcher> matchesMiddle = [IdentityMatcher forView:middle];
-
-    ComplexMatcher *middleInRoot = [ComplexMatcher withHead:matchesRoot subject:matchesMiddle];
+- (void)testSubjectMatchesViewHeadMatchesParent {
+    ComplexMatcher *middleInRoot = [ComplexMatcher withHead:_matchesRoot subject:_matchesMiddle];
 
     assertThat(middleInRoot, [MatchesView view:middle inTree:root]);
 }
 
-- (void)testMatchingSubjectMismatchingHead {
-    id<SubjectMatcher> neverMatches = [FalseMatcher new];
-    id<SubjectMatcher> matchesMiddle = [IdentityMatcher forView:middle];
+- (void)testSubjectMatchesViewHeadMatchesAncestor {
+    ComplexMatcher *leafInRoot = [ComplexMatcher withHead:_matchesRoot subject:_matchesLeaf];
 
-    ComplexMatcher *middleInNonExistent = [ComplexMatcher withHead:neverMatches subject:matchesMiddle];
+    assertThat(leafInRoot, [MatchesView view:leaf inTree:root]);
+}
+
+- (void)testSubjectMatchesViewHeadMismatchesAllAncestors {
+    id<SubjectMatcher> neverMatches = [FalseMatcher new];
+
+    ComplexMatcher *middleInNonExistent = [ComplexMatcher withHead:neverMatches subject:_matchesMiddle];
 
     assertThat(middleInNonExistent, isNot([MatchesView view:middle inTree:root]));
+}
+
+- (void)testComparesHeadToViewsUpToGivenRoot {
+    ComplexMatcher *leafInMiddle = [ComplexMatcher withHead:_matchesMiddle subject:_matchesLeaf];
+
+    assertThat(leafInMiddle, [MatchesView view:leaf inTree:middle]);
+}
+
+- (void)testDoesNotCompareHeadToViewsAboveGivenRoot {
+    ComplexMatcher *leafInRoot = [ComplexMatcher withHead:_matchesRoot subject:_matchesLeaf];
+
+    assertThat(leafInRoot, isNot([MatchesView view:leaf inTree:middle]));
+}
+
+- (void)testMatchesAcrossUniversalMatcher {
+    id<SubjectMatcher> universalMatcher = [UniversalMatcher new];
+    id<SubjectMatcher> matchesAnyViewInRoot = [ComplexMatcher withHead:_matchesRoot subject:universalMatcher];
+    id<SubjectMatcher> leafInAnyViewInRoot = [ComplexMatcher withHead:matchesAnyViewInRoot subject:_matchesLeaf ];
+
+    assertThat(leafInAnyViewInRoot, [MatchesView view:leaf inTree:root]);
 }
 
 @end
