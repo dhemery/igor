be6617ad78b2a749ecd97e011810f70d39751e7b
Flip Sasser <flip@inthebackforty.com>
2013-03-26 14:18:55 -0400

    OCHamcrest running for iOS project

---
+++ IgorTests/IgorTests.h
@@ -0,0 +1,13 @@
+//
+//  IgorTests.h
+//  IgorTests
+//
+//  Created by Flip Sasser on 3/26/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface IgorTests : SenTestCase
+
+@end
---
+++ IgorTests/IgorTests.m
@@ -0,0 +1,32 @@
+//
+//  IgorTests.m
+//  IgorTests
+//
+//  Created by Flip Sasser on 3/26/13.
+//
+//
+
+#import "IgorTests.h"
+
+@implementation IgorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in IgorTests");
+}
+
+@end
